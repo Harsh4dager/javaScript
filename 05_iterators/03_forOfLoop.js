235408a76064a320ab9068d1c009e3be29ed4df7
Harsh <102403225+Harsh4dager@users.noreply.github.com>
2024-01-30 15:27:00 +0000

    for of loop in js

---
+++ 05_iterators/03_forOfLoop.js
@@ -0,0 +1,16 @@
+const places = ["ram mandir", "taj mahal", "india gate", "gateway of india"]
+
+for(let place of places) console.log(place);
+
+// can also run this on other iterables as well
+const name = "harsh dager"
+
+for(let char of name){
+    console.log(char)
+}
+
+// let's talk about Map object: 
+// it is a js object which hold key value pairs and 
+// also remembers the original insertion order
+
+
