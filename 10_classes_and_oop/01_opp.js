5b4fb2c9096cf2d5f56d052bc54741e5086b50d3
Harsh <102403225+Harsh4dager@users.noreply.github.com>
2024-02-13 11:39:12 +0530

    intro to oops in javaScipt

---
+++ 10_classes_and_oop/01_opp.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+// let's see the uses of constructor function
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+// have to use new otherwise everytime it will overwrite
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+// things about new keyword
+/* 
+1. when you use a new keyword a empty object got created which we call instance/object
+2. a constructor function is called due to this new keyword( which packs all the arguments and give them to you )
+3. these arguments got injected into this empty object( this keyword )
+*/
